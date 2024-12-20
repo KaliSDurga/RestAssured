61282751f1703cd6c9625de32db93980852f8644
Sahasra <laks.kali99@gmail.com>
2024-12-20 21:15:52 +0530

    Added files for TestNG

--- src/test/java/com/example/ex_ra01/Lab260.java
+++ src/test/java/com/example/ex_ra01/Lab260.java
@@ -1,4 +1,13 @@
 package com.example.ex_ra01;
 
+import io.restassured.RestAssured;
+
 public class Lab260 {
+    public static void main(String[] args) {
+        RestAssured.given()
+                .baseUri("https://restful-booker.herokuapp.com/")
+                .basePath("/ping")
+                .when().get()
+                .then().statusCode(200);
+    }
 }
